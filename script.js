2b93cd35c37489d468ec5dda53a4e7eb584b6988
Satish maurya <satishmaurya15216@gmail.com>
2021-10-22 09:05:37 +0530

    added ticket

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
 let addbtn=document.querySelector(".add-btn");
 let modal=document.querySelector(".modal-container");
 
+let maincontainer= document.querySelector(".main-container");
+let textareacontainer= document.querySelector(".text-area-container");
+
 addbtn.addEventListener("click",()=>{
     modal.classList.toggle("active");
 })
+
+
+modal.addEventListener("keydown",(e)=>{
+    let key = e.key;
+    if(key === "m"){
+        createTicket();
+        modal.classList.remove("active");
+        textareacontainer.innerText= "";
+    }
+})
+
+function createTicket(){
+    let ticketContainer=document.createElement("div");
+    ticketContainer.setAttribute("class","ticket-container");
+    ticketContainer.innerHTML= `<div class="ticket-color"></div>
+    <div class="ticket-id">sample-id</div>
+    <div class="task-area">Lorem ipsum dolor sit amet consectetur adipisicing elit.</div>`;
+
+    maincontainer.appendChild(ticketContainer);
+}
+
