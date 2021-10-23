25002f847c14d73b5171a164c8493c3727a7005c
Satish maurya <satishmaurya15216@gmail.com>
2021-10-23 21:43:00 +0530

    added priority color

--- script.js
+++ script.js
@@ -3,20 +3,44 @@ let modal=document.querySelector(".modal-container");
 
 let maincontainer= document.querySelector(".main-container");
 let textareacontainer= document.querySelector(".text-area-container");
+let colors = ["rgb(247, 140, 140)","rgb(152, 250, 152)","rgb(163, 163, 255)","rgb(70, 69, 69)"];
+let modalprioritycolor=colors[colors.length-1];
+
+let prioritycolors=document.querySelectorAll(".priority-color");
+
+prioritycolors.forEach((colorelem ,index) => {
+
+    colorelem.addEventListener("click",() => {
+
+        prioritycolors.forEach((prioritycolorelem ,index) => {
+                prioritycolorelem.classList.remove("border");
+        
+        });
+    });
+    
+        colorelem.classList.add("border");
+});
+
+
 
 addbtn.addEventListener("click",()=>{
     modal.classList.toggle("active");
-})
+});
+
+
 
 
 modal.addEventListener("keydown",(e)=>{
     let key = e.key;
-    if(key === "m"){
+   
+    if(key === 'Shift'){
         createTicket();
         modal.classList.remove("active");
         textareacontainer.innerText= "";
     }
-})
+});
+
+
 
 function createTicket(){
     let ticketContainer=document.createElement("div");
@@ -26,5 +50,5 @@ function createTicket(){
     <div class="task-area">Lorem ipsum dolor sit amet consectetur adipisicing elit.</div>`;
 
     maincontainer.appendChild(ticketContainer);
-}
+};
 
