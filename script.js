6072ae754f79e6f95073d4380cfbdd451d9c5479
Satish maurya <satishmaurya15216@gmail.com>
2021-10-25 09:23:40 +0530

    added lock functionality to oue tickets

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 let addbtn=document.querySelector(".add-btn");
+let removebtn=document.querySelector(".remove-btn");
+
+
 let modal=document.querySelector(".modal-container");
 
 let maincontainer= document.querySelector(".main-container");
@@ -12,8 +15,10 @@ let prioritycolors=document.querySelectorAll(".priority-color");
 
 
 
+let removeFlag = false;
 
-
+let lockClass = "fa-lock";
+let unlockClass = "fa-lock-open";
 
 
 prioritycolors.forEach((colorelem ,index) => {
@@ -39,6 +44,9 @@ addbtn.addEventListener("click",()=>{
 });
 
 
+removebtn.addEventListener("click",()=>{
+    removeFlag = !removeFlag;
+})
 
 
 modal.addEventListener("keydown",(e)=>{
@@ -59,8 +67,41 @@ function createTicket(ticketColor,ticketTask,ticketid){
     ticketContainer.innerHTML= `<div class="ticket-color ${ticketColor}"></div>
     <div class="ticket-id"> # ${ticketid}</div>
     <div class="task-area">${ticketTask}</div>
-    `;
+    <div class="ticket-lock">
+    <i class="fas fa-lock"></i>
+</div> `;
 
     maincontainer.appendChild(ticketContainer);
+  
+    handleRemoval(ticketContainer);
+
+    handleLock(ticketContainer);
+   
 };
 
+
+function handleRemoval(ticket){
+
+    if(removeFlag) ticket.remove();
+
+}
+
+function handleLock(ticket){
+    let ticketlockElem=ticket.querySelector(".ticket-lock");
+  let  ticketlock = ticketlockElem.children[0];
+  ticketlock.addEventListener("click",()=>{
+      if(ticketlock.classList.contains(lockClass)){
+
+        ticketlock.classList.remove(lockClass);
+        ticketlock.classList.add(unlockClass);
+
+
+      }else{
+        
+        
+        ticketlock.classList.remove(unlockClass);
+        ticketlock.classList.add(lockClass);
+
+      }
+  })
+}
